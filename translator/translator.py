67777cccc4279d8407bbe92ee090bf443bbba94b
Thomas Muscarello <muscarello.thomas@gmail.com>
2021-06-07 18:20:14 -0400

    translator

---
+++ translator/translator.py
@@ -0,0 +1,23 @@
+#Made Up Language
+#All vowels (a,e,i,o,u) change to "oo"
+
+#fish -> foosh
+#dog -> doog
+#--------------------------------------#
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            #Allows to keep uppercase is a vowel starts the phrase
+            if letter.isupper():
+                translation = translation + "Oo"
+            else:
+                translation= translation + "oo" 
+        else:
+            translation=translation + letter
+    return translation
+
+print(
+    translate(input("Enter a Phrase: "))
+)
