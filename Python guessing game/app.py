d8a736b15eef5763b42783b779ffa6eaf3e1073c
Thomas Muscarello <muscarello.thomas@gmail.com>
2021-07-06 18:28:21 -0400

    multiple choice game

---
+++ Python guessing game/app.py
@@ -0,0 +1,5 @@
+question_prompts = [
+    "What color are Bananas?\n(a) Yellow\n (b) Purple\n (c) Blue",
+    "What color combo is the American Flag?\n(a) Yellow, Red, Black\n (b) Red, White, Blue\n (c) White, Black, Green",
+    "What color is the Ocean?\n(a) Yellow\n (b) Purple\n (c) Blue"
+]
