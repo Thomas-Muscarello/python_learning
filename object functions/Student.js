2d248b93f2ad815a50c4d5a0f408096117e0d486
Thomas Muscarello <muscarello.thomas@gmail.com>
2021-07-15 22:21:04 -0400

    added year

--- object functions/Student.js
+++ object functions/Student.js
@@ -1,7 +1,8 @@
 class Student{
-    constructor(self, name, major, gpa){
+    constructor(self, name, major, gpa, year){
         self.name = name
         self.major = major
         self.gpa = gpa
+        self.year = year
     }
 }
