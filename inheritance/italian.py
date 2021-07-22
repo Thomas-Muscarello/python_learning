278856121a13797e72c87b043deff18477724219
Thomas Muscarello <muscarello.thomas@gmail.com>
2021-07-22 17:22:07 -0400

    fxing bugs

--- inheritance/italian.py
+++ inheritance/italian.py
@@ -4,4 +4,4 @@ class Italian(Chef):
     #Now the italian chef can do eveything the normal chef can do.
     #You can also create another class for just the italian chef.
     def make_pasta(self):
-        print("The italian chef makes a pasta dish! ")
+        print("The italian chef makes a pasta dish!")
